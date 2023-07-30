b4965d53c9410a514e43dcd7b3c3ed4f9e1687fd
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 20:19:15 +0300

    squash!

--- dist/lib/decrypt.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (data: Uint8Array, Key: string, iv: Uint8Array) => Promise<Uint8Array>;
-export default _default;
--- dist/lib/encrypt.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (data: unknown, Key: string) => Promise<{
-    iv: Uint8Array;
-    data: Uint8Array;
-}>;
-export default _default;
