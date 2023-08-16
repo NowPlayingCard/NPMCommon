7e8da8f13a929a9621f5801f5f3e2931cc47858b
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-17 01:34:53 +0300

    squash!

--- Target/Index.d.ts
+++ Target/Index.d.ts
@@ -2,4 +2,3 @@ export { default as Access } from "./Library/Access.js";
 export { default as Decrypt } from "./Library/Decrypt.js";
 export { default as Encrypt } from "./Library/Encrypt.js";
 export { default as Uint8ArrayFromObject } from "./Library/Uint8ArrayFromObject.js";
-
