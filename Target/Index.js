b72ed4ea8e2024faf2e2418d98c9c9769a471754
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-16 14:29:16 +0300

    squash!

--- Target/Index.js
+++ Target/Index.js
@@ -1,1 +1,6 @@
-import{default as e}from"./Library/Access.js";import{default as o}from"./Library/Decrypt.js";import{default as p}from"./Library/Encrypt.js";import{default as c}from"./Library/Uint8ArrayFromObject.js";export{e as Access,o as Decrypt,p as Encrypt,c as Uint8ArrayFromObject};
+import { default as e } from "./Library/Access.js";
+import { default as o } from "./Library/Decrypt.js";
+import { default as p } from "./Library/Encrypt.js";
+import { default as c } from "./Library/Uint8ArrayFromObject.js";
+export { e as Access, o as Decrypt, p as Encrypt, c as Uint8ArrayFromObject };
+
