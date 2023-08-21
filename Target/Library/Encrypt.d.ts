88cd9fd81ee24c45067caaecbde01c879de5ae2e
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-21 16:51:14 +0300

    squash!

--- Target/Library/Encrypt.d.ts
+++ Target/Library/Encrypt.d.ts
@@ -1,3 +1,15 @@
+/**
+ * The Encrypt function encrypts data using the AES-GCM algorithm with a given key.
+ * @param {unknown} Data - The `Data` parameter is the data that you want to encrypt. It
+ * can be of any type, but it will be converted to a JSON string before encryption.
+ * @param {string} Key - The `Key` parameter is a string that represents the encryption
+ * key used to encrypt the data. It is used to derive a symmetric encryption key that is
+ * compatible with the AES-GCM algorithm.
+ * @returns The `Encrypt` function returns an object with two properties: `IV` and
+ * `Data`. `IV` is a randomly generated 12-byte array used as the initialization vector
+ * for the encryption. `Data` is the encrypted version of the input `Data` using the
+ * AES-GCM encryption algorithm.
+ */
 declare const _default: (Data: unknown, Key: string) => Promise<{
     IV: Uint8Array;
     Data: Uint8Array;
