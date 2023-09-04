ef6e5fee62fb660edcfe949dd2db69ee47432c84
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 19:11:26 +0300

    squash!

--- Target/Index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as Access } from "./Library/Access.js";
-export { default as Decrypt } from "./Library/Decrypt.js";
-export { default as Encrypt } from "./Library/Encrypt.js";
-export { Get } from "./Library/Get.js";
-export { Put } from "./Library/Put.js";
-export { default as Uint8ArrayFromObject } from "./Library/Uint8ArrayFromObject.js";
--- Target/Library/Access.js
+++
@@ -1,1 +0,0 @@
-import{Buffer as i}from"node:buffer";import c from"./Decrypt.js";import t from"./Uint8ArrayFromObject.js";var f=async(a,e,o,m)=>{try{const{IV:r,Data:p}=await o.get(e,{type:"json"});return JSON.parse(i.from(await c(await t(p),a??"",await t(r))).toString())[m]}catch{}};export{f as default};
