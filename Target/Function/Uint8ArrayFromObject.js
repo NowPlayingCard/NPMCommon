de9633987ba26afcfb8e18c07b54a1e4fe88583b
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 07:24:28 +0300

    squash!

--- Target/Function/Uint8ArrayFromObject.js
+++ Target/Function/Uint8ArrayFromObject.js
@@ -1,13 +1,1 @@
-var c = async (e) => {
-	const a = Object.keys(e)
-			.map(Number)
-			.sort((r, t) => r - t),
-		n = new Uint8Array(a.length);
-	return (
-		a.forEach((r, t) => {
-			n[t] = e[r] ?? 0;
-		}),
-		n
-	);
-};
-export { c as default };
+var c=async e=>{const a=Object.keys(e).map(Number).sort((r,t)=>r-t),n=new Uint8Array(a.length);return a.forEach((r,t)=>{n[t]=e[r]??0}),n};export{c as default};
