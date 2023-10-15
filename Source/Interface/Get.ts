939abf4553181c1770e522a57e9d7c10a57caaad
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-15 13:19:54 +0300

    squash!

--- Source/Interface/Get.ts
+++ Source/Interface/Get.ts
@@ -5,12 +5,12 @@
 export default interface Type {
 	/**
 	 * The Get function recursively converts an object into a Map data structure.
-	 * 
+	 *
 	 * @param Instance - The `Instance` parameter is an object that contains key-value pairs. The keys can
 	 * be any string, and the values can be of any type.
-	 * 
+	 *
 	 * @returns a Map object containing the key-value pairs from the input object.
-	 * 
+	 *
 	 */
 	(Instance: {
 		// biome-ignore lint/suspicious/noExplicitAny:
