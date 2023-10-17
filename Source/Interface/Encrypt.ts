dcb5e31808113bce7b6f452b979b62dbb7e3c755
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-17 19:09:32 +0300

    squash!

--- Source/Interface/Encrypt.ts
+++ Source/Interface/Encrypt.ts
@@ -6,10 +6,10 @@ export default interface Type {
 	/**
 	 * The Encrypt function encrypts data using the AES-GCM algorithm with a given key.
 	 *
-	 * @param {unknown} Data - The `Data` parameter is the data that you want to encrypt. It
+	 * @param Data - The `Data` parameter is the data that you want to encrypt. It
 	 * can be of any type, but it will be converted to a JSON string before encryption.
 	 *
-	 * @param {string} Key - The `Key` parameter is a string that represents the encryption
+	 * @param Key - The `Key` parameter is a string that represents the encryption
 	 * key used to encrypt the data. It is used to derive a symmetric encryption key that is
 	 * compatible with the AES-GCM algorithm.
 	 *
--- Target/Interface/Access.d.ts
+++ Target/Interface/Access.d.ts
@@ -16,11 +16,11 @@ export default interface Type {
      * `randomUUID` function from the `Crypto` object. It is used to retrieve data from the
      * KV namespace.
      *
-     * @param {KVNamespace} KV - KV is an instance of the KVNamespace class, which is used
+     * @param KV - KV is an instance of the KVNamespace class, which is used
      * to interact with the Workers KV storage. It provides methods for reading, writing,
      * and deleting data from the KV storage.
      *
-     * @param {string} View - The `View` parameter is a string that represents the specific
+     * @param View - The `View` parameter is a string that represents the specific
      * view or property of the decrypted data that you want to access. It is used to
      * retrieve a specific value from the decrypted data object.
      *
