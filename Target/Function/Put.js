e2744b4aeac929e6fc7efc59c967e5d4e7ab35e9
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-26 23:48:45 +0300

    squash!

--- Target/Function/Put.js
+++ Target/Function/Put.js
@@ -1,1 +1,1 @@
-const s=(...[e])=>{if(e instanceof Map){const t={};for(const[o,r]of e.entries())r instanceof Map?t[o]=s(r):t[o]=r;return t}return e};var a=s;export{s as _Function,a as default};
+const a=(...[e])=>{if(e instanceof Map){const t={};for(const[s,r]of e.entries())r instanceof Map?t[s]=a(r):t[s]=r;return t}return e};var n=a;export{a as _Function,n as default};
